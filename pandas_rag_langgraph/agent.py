1759b1f0d49f5ba0df8338cf375747c2e3fd31cf
vbarda <vadym@langchain.dev>
2024-06-25 09:52:58 -0400

    remove grading

--- pandas_rag_langgraph/agent.py
+++ pandas_rag_langgraph/agent.py
@@ -65,35 +65,12 @@ def get_retriever() -> BaseRetriever:
 
 # LLM / Retriever
 llm = ChatOpenAI(model="gpt-4o", temperature=0)
-retrieval_grader_llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 retriever = get_retriever()
 
 
 # Prompts / data models
 
 RAG_PROMPT: ChatPromptTemplate = hub.pull("rlm/rag-prompt")
-
-class GradeDocuments(BaseModel):
-    """Binary score for relevance check on retrieved documents."""
-
-    binary_score: str = Field(
-        description="Documents are relevant to the question, 'yes' or 'no'"
-    )
-
-RETRIEVAL_GRADER_SYSTEM = (
-"""
-You are a grader assessing relevance of a retrieved document to a user question.
-Your goal is to filter out erroneous retrievals.
-If the document contains keyword(s) or semantic meaning related to the user question, grade it as relevant.
-Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question.
-"""
-)
-RETRIEVAL_GRADER_PROMPT = ChatPromptTemplate.from_messages(
-    [
-        ("system", RETRIEVAL_GRADER_SYSTEM),
-        ("human", "Retrieved document: \n\n {document} \n\n User question: {question}"),
-    ]
-)
 
 
 class GradeHallucinations(BaseModel):
@@ -219,32 +196,6 @@ def generate(state: GraphState, config):
     return state_update
 
 
-def grade_documents(state: GraphState):
-    """
-    Determines whether the retrieved documents are relevant to the question.
-
-    Args:
-        state (dict): The current graph state
-
-    Returns:
-        state (dict): Updates documents key with only filtered relevant documents
-    """
-    print("---CHECK DOCUMENT RELEVANCE TO QUESTION---")
-    question = state["question"]
-    documents = state["documents"]
-    retrieval_grader = RETRIEVAL_GRADER_PROMPT | retrieval_grader_llm.with_structured_output(GradeDocuments)
-
-    # Score each doc
-    filtered_docs = []
-    grades: list[GradeDocuments] = retrieval_grader.batch([
-        {"question": question, "document": d.page_content}
-        for d in documents
-    ])
-    filtered_docs = [doc for doc, grade in zip(documents, grades) if grade.binary_score == "yes"]
-    print(f"---GRADE: {len(filtered_docs)}/{len(documents)} RELEVANT DOCUMENTS---")
-    return {"documents": filtered_docs}
-
-
 def transform_query(state: GraphState):
     """
     Transform the query to produce a better question.
@@ -269,35 +220,7 @@ def transform_query(state: GraphState):
 ### Edges
 
 
-def decide_to_generate(state: GraphState, config) -> Literal["transform_query", "generate"]:
-    """
-    Determines whether to generate an answer, or re-generate a question.
-
-    Args:
-        state (dict): The current graph state
-
-    Returns:
-        str: Binary decision for next node to call
-    """
-    print("---ASSESS GRADED DOCUMENTS---")
-    filtered_documents = state["documents"]
-    retries = state["retries"] if state.get("retries") is not None else -1
-    max_retries = config.get("configurable", {}).get("max_retries", 3)
-
-    if not filtered_documents and retries < max_retries:
-        # All documents have been filtered check_relevance
-        # We will re-generate a new query
-        print(
-            "---DECISION: ALL DOCUMENTS ARE NOT RELEVANT TO QUESTION, TRANSFORM QUERY---"
-        )
-        return "transform_query"
-    else:
-        # We have relevant documents or we ran out of retries, so generate answer
-        print("---DECISION: GENERATE---")
-        return "generate"
-
-
-def grade_generation_v_documents_and_question(state: GraphState) -> Literal["not enough information", "not grounded", "useful", "not useful"]:
+def grade_generation_v_documents_and_question(state: GraphState):
     """
     Determines whether the generation is grounded in the document and answers question.
 
@@ -312,7 +235,7 @@ def grade_generation_v_documents_and_question(state: GraphState) -> Literal["not
     generation = convert_to_messages(state["messages"])[-1]
 
     if generation.content == NOT_ENOUGH_INFORMATION:
-        return "not enough information"
+        return END
 
     print("---CHECK HALLUCINATIONS---")
     hallucination_grader = HALLUCINATION_GRADER_PROMPT | llm.with_structured_output(GradeHallucinations)
@@ -323,7 +246,7 @@ def grade_generation_v_documents_and_question(state: GraphState) -> Literal["not
     # Check hallucination
     if hallucination_grade.binary_score != "yes":
         print("---DECISION: GENERATION IS NOT GROUNDED IN DOCUMENTS, RE-TRY---")
-        return "not grounded"
+        return "generate"
 
     print("---DECISION: GENERATION IS GROUNDED IN DOCUMENTS---")
 
@@ -334,10 +257,10 @@ def grade_generation_v_documents_and_question(state: GraphState) -> Literal["not
     answer_grade: GradeAnswer = answer_grader.invoke({"question": question, "generation": generation.content})
     if answer_grade.binary_score == "yes":
         print("---DECISION: GENERATION ADDRESSES QUESTION---")
-        return "useful"
+        return END
     else:
         print("---DECISION: GENERATION DOES NOT ADDRESS QUESTION---")
-        return "not useful"
+        return "transform_query"
 
 
 # Define graph
@@ -346,25 +269,17 @@ workflow = StateGraph(GraphState)
 
 # Define the nodes
 workflow.add_node("retrieve", retrieve)
-workflow.add_node("grade_documents", grade_documents)
 workflow.add_node("generate", generate)
 workflow.add_node("transform_query", transform_query)
 
 # Build graph
 workflow.set_entry_point("retrieve")
-workflow.add_edge("retrieve", "grade_documents")
-workflow.add_conditional_edges("grade_documents", decide_to_generate)
+workflow.add_edge("retrieve", "generate")
 workflow.add_edge("transform_query", "retrieve")
 
 workflow.add_conditional_edges(
     "generate",
-    grade_generation_v_documents_and_question,
-    {
-        "not grounded": "generate",
-        "not useful": "transform_query",
-        "useful": END,
-        "not enough information": END
-    },
+    grade_generation_v_documents_and_question
 )
 
 # Compile
